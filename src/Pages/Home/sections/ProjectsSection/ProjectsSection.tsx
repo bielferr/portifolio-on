c2ef92a889140c92ea4747796f9051055179e4a8
bielferr <gabrielferreiradesouza5513@gmail.com>
2025-04-12 13:39:32 -0300

    subindo de assets para public

--- src/Pages/Home/sections/ProjectsSection/ProjectsSection.tsx
+++ src/Pages/Home/sections/ProjectsSection/ProjectsSection.tsx
@@ -13,7 +13,7 @@ const ProjectsSection: React.FC = () => {
         {
             title: "Project Exemple",
             subtitle: "março 2025 - março 2025",
-            srcImg: "src/assets/images/system.png",
+            srcImg: "/images/system.png",
             description: "Sistema simples para gerenciar usuários. Este projeto permite adicionar, visualizar e excluir usuários de um sistema. Ele foi criado com o propósito de praticar conceitos básicos de programação e controle de dados.",
             technologies: "Technologies: JavaScript, HTML,CSS",
             websiteURL: "https://github.com/bielferr/systemOfUsers",
@@ -22,7 +22,7 @@ const ProjectsSection: React.FC = () => {
         {
             title: "Gerador de qr code",
             subtitle: "maio 2024 - maio 2024",
-            srcImg: "src/assets/images/gerador-qr-code.png",
+            srcImg: "/images/gerador-qr-code.png",
             description: "este é um gerador de qrcode gratuito para ajudar as pessoas",
             technologies: "Technologies: JavaScript, HTML, CSS, ",
             websiteURL: "https://qrgerador.netlify.app/",
@@ -31,7 +31,7 @@ const ProjectsSection: React.FC = () => {
         {
             title: "crudify-tasks",
             subtitle: "out 2024 - nov 2024",
-            srcImg: "src/assets/images/server.png",
+            srcImg: "/images/server.png",
             description: "Servidor Node.js - Projeto de Tarefas (Back-nd) Este projeto é um servidor desenvolvido em Node.js para gerenciar operações básicas com tarefas (CRUD). O projeto inclui a manipulação de streams, desestruturação de objetos, gerenciamento de estados e um sistema básico de rotas.",
             technologies: "Technologies: JavaScript",
             websiteURL: "",
@@ -40,23 +40,23 @@ const ProjectsSection: React.FC = () => {
         {
             title: "Heart for baby",
             subtitle: "Jul 2019 - May 2019",
-            srcImg: "src/assets/images/coracao.png",
+            srcImg: "/images/coracao.png",
             description: "Projeto para namorados e namoradas presentearem seus amados",
             technologies: "Technologies: JavaScript, HTML, CSS",
             websiteURL: "https://datinghappy.netlify.app/",
             codeURL: "https://github.com/bielferr/heart-baby",
         },
-
         {
             title: "FlowBank",
             subtitle: "nov 2024 - dez 2024",
-            srcImg: "src/assets/images/flow.png",
+            srcImg: "/images/flow.png",
             description: " API REST de transações com autenticação, controle por usuário e testes automatizado",
             technologies: "Technologies: TypeScript, HTML, CSS, MYSQL",
             websiteURL: "",
             codeURL: "https://github.com/bielferr/Api-rest-node",
         },
     ]
+    
 
     return (
         <StyledExperience>
